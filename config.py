bf7c7dccb17cceae081aa703f7ecfdc8e027a8e5
T55-100 <3078286809@qq.com>
2025-11-30 10:50:12 +0800

    1

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
 # 服务器地址列表，后期可在前端配置
 SERVERS = [
     {'name': '本地服务器', 'address': 'http://127.0.0.1:5000'},
-    {'name': '服务器1', 'address': 'http://192.168.1.100:5000'},
-    {'name': '服务器2', 'address': 'http://192.168.1.101:5000'}
+    {'name': '隧道1', 'address': 'http://103.45.130.80:59043'},
+    {'name': '隧道2', 'address': 'http://frp-tag.com:59043'}
 ]
